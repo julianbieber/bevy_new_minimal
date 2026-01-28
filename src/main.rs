1d4396359416369e2b070190b389813c8be456ff
Julian Bieber <j.bieber@omniaretail.com>
2026-01-28 22:06:45 +0100

    quit button and help in main menu

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-use bevy::prelude::*;
+use bevy::{
+    feathers::{
+        FeathersPlugins,
+        controls::{ButtonProps, button},
+        dark_theme::create_dark_theme,
+        theme::{ThemeBackgroundColor, UiTheme},
+        tokens,
+    },
+    prelude::*,
+    ui_widgets::{Activate, observe},
+};
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins((DefaultPlugins, FeathersPlugins, ScreenPlugin))
+        .run()
 }
 
 pub struct ScreenPlugin;
@@ -17,6 +29,7 @@ impl Plugin for ScreenPlugin {
 pub enum Screen {
     #[default]
     Main,
+    Help,
     Gameplay,
 }
 
@@ -26,24 +39,72 @@ pub struct MainScreenPlugin;
 
 impl Plugin for MainScreenPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(UiTheme(create_dark_theme()));
         app.add_systems(OnEnter(Screen::Main), setup_ui);
+        app.add_systems(OnEnter(Screen::Help), setup_help);
     }
 }
 
 fn setup_ui(mut commands: Commands) {
     commands.spawn(Camera2d);
+    commands.spawn(main_root());
 }
 
+/// 3 Buttons:
+/// * Play
+/// * Help
+/// * Quit
 fn main_root() -> impl Bundle {
     (
+        DespawnOnExit(Screen::Main),
         Node {
+            display: Display::Flex,
+            flex_direction: FlexDirection::Column,
             width: percent(100),
             height: percent(100),
-            display: Display::Flex,
+            row_gap: px(10),
             ..Default::default()
         },
-        children![],
+        ThemeBackgroundColor(tokens::WINDOW_BG),
+        children![
+            (
+                button(ButtonProps::default(), (), Spawn(Text::new("Play!"))),
+                observe(|_: On<Activate>| { info!("play pressed") }),
+            ),
+            (
+                button(ButtonProps::default(), (), Spawn(Text::new("Help"))),
+                observe(go_to_help),
+            ),
+            (
+                button(ButtonProps::default(), (), Spawn(Text::new("Quit"))),
+                observe(quit),
+            )
+        ],
     )
+}
+
+fn go_to_help(_: On<Activate>, mut next: ResMut<NextState<Screen>>) {
+    next.set(Screen::Help);
+}
+
+fn setup_help(mut commands: Commands) {
+    commands.spawn((
+        DespawnOnExit(Screen::Help),
+        Node {
+            display: Display::Flex,
+            flex_direction: FlexDirection::Column,
+            width: percent(100),
+            height: percent(100),
+            row_gap: px(10),
+            ..Default::default()
+        },
+        ThemeBackgroundColor(tokens::WINDOW_BG),
+        children![Text::new("Some text to explain how to play the game")],
+    ));
+}
+
+fn quit(_: On<Activate>, mut commands: Commands) {
+    commands.write_message(AppExit::Success);
 }
 
 ////////////////
@@ -51,7 +112,5 @@ fn main_root() -> impl Bundle {
 pub struct GameplayPlugin;
 
 impl Plugin for GameplayPlugin {
-    fn build(&self, _app: &mut App) {
-        todo!()
-    }
+    fn build(&self, _app: &mut App) {}
 }
